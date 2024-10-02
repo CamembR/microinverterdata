9c25fcaa53c5327abf7aaf79058444a7f0110470
C. Regouby <christophe.regouby@free.fr>
2024-10-02 19:59:44 +0200

    use check_device_ip()

--- R/query_device.R
+++ R/query_device.R
@@ -13,7 +13,7 @@
 #' query_ap_device(device_ip = "192.168.0.234", query = "getDeviceInfo")
 #' }
 query_ap_device <- function(device_ip, query) {
-  stopifnot("device_IP shall be an atomic character string" = length(device_ip) == 1)
+  check_device_ip(device_ip)
   url <- glue::glue("http://{device_ip}:8050/{query}")
   req <- request(url)
   resp <- req |> req_perform()
@@ -58,6 +58,7 @@ query_ap_devices <- function(device_ip, query) {
   }
 }
 
+
 #' Enphase single device query
 #'
 #' as a port of https://github.com/mr-manuel/venus-os_dbus-enphase-envoy/tree/master#json-structure
@@ -79,7 +80,7 @@ query_ap_devices <- function(device_ip, query) {
 #' query_enphase_device(device_ip = "192.168.0.234", query = "production/inverters/")
 #' }
 query_enphase_device <- function(device_ip = "enphase.local", query, username = Sys.getenv("ENPHASE_USERNAME"), password = Sys.getenv("ENPHASE_PASSWORD")) {
-  stopifnot("device_IP shall be an atomic character string" = length(device_ip) == 1)
+  check_device_ip(device_ip)
   url <- glue::glue("http://{device_ip}/api/v1/{query}")
   req <- request(url) |> req_auth_basic(username, password)
   resp <- req |> req_perform()
@@ -98,3 +99,10 @@ query_enphase_device <- function(device_ip = "enphase.local", query, username = 
   }
 }
 
+
+check_device_ip <- function(device_ip) {
+  stopifnot("device_IP shall be an atomic character string" = length(device_ip) == 1)
+  stopifnot("device_IP shall be of a minimal character length" = nchar(device_ip) >= 3)
+
+
+}
