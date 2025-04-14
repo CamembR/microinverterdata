dc332194e56565c0f815259ad479f093eca63122
C. Regouby <christophe.regouby@free.fr>
2025-04-14 14:28:20 +0200

    fix local mocked bindings

--- tests/testthat/test-get_device_info.R
+++ tests/testthat/test-get_device_info.R
@@ -1,3 +1,28 @@
+
+local_mocked_bindings(.req_perform_parallel = function(requests, ...) {
+  lapply(requests, httr2::req_perform)
+})
+
+local_mocked_bindings(check_device_ip = function(device_ip) {
+  if (rlang::enexpr(device_ip) %in% c("apsystems_host", "apsystems_multi", "fronius", "fronius2", "fronius3")) {
+    return
+  } else {
+    stopifnot("**device_ip** shall be an atomic character string" = is.atomic(device_ip))
+    stopifnot("**device_ip** shall be of a minimal character length" = nchar(device_ip) >= 3)
+
+    # Regular expression to match a valid IPv4 address
+    ipv4_regex <- "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    # Regular expression to match a valid IPv6 address
+    ipv6_regex <- "^[0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){7}$"
+    # Regular expression to match a .local domain resolution
+    local_regex <- "\\.local$"
+
+    stopifnot("**device_ip** shall be a valid .local, IPv4 or IPv6 address" = grepl(ipv4_regex, device_ip) || grepl(ipv6_regex, device_ip) || grepl(local_regex, device_ip))
+
+  }
+})
+
+
 with_mock_dir("apsystems", {
 
     test_that("get_device_info() works with a single device from APSystems", {
